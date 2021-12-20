f9d165f07da89d30dce726649a4be79272ee69a5
Domenico <castronovo.d7@gmail.com>
2021-12-20 17:14:20 +0100

    daily data upload

--- apps/accuweather.py
+++ apps/accuweather.py
@@ -15,19 +15,25 @@ from app import app
 
 import plotly.graph_objects as go
 
-#%% Page Structure
+#%% Page Structure and TODO
 
 '''
-    Headline
-city filter
-comparison plot
-historical plot
+Structure and TODOs
 
-TODO:
-    insert start and end date
+- Headline
+- Description explaining what they are seeing and why I did it.
+- city filter | DateTime Filter.
+- comparison plot.
+- historical plot (?) Historical is already in comparison plot. Maybe pull more historical if api allows.
+- Table of errors and more statistics.
+- Map of with MAE.
+
 '''
+#%% Alpha ISO 3
 
-#%% Allign DataFrames
+iso = {'Athens':'GRC', 'Berlin':'DEU', 'Paris':'FRA', 'Rome':'ITA'}
+
+#%% Prepare DataFrames
 
 # get relative data folder
 PATH = pathlib.Path(__file__).parent
@@ -59,45 +65,56 @@ df_merged.fc_query = df_merged.fc_query.ffill()
 
 layout = html.Div([
     html.H1('Accuweather Project', style={"textAlign": "center"}),
-
+    html.H1('///', style={"textAlign": "center"}), 
     html.Div([
         html.Div(dcc.Dropdown(
             id='city-dropdown', value='Athens', clearable=False,
-            options=[{'label': x, 'value': x} for x in sorted([i for i in dfh.columns])]
+            options=[{'label': x, 'value': x} for x in sorted([i for i in dfh.columns])],
+            multi=False
         ), className='six columns'),
     
-    
-    
-    #     html.Div(dcc.Dropdown(
-    #         id='sales-dropdown', value='EU Sales', clearable=False,
-    #         persistence=True, persistence_type='memory',
-    #         options=[{'label': x, 'value': x} for x in sales_list]
-    #     ), className='six columns'),
         
         html.Div(dcc.DatePickerRange(id='date-range',
-                                     min_date_allowed=dff.index.min(),
+                                     min_date_allowed=dfh.index.min(),
                                      max_date_allowed=dff.index.max(),
                                      initial_visible_month=dff.index.min(),
                                      end_date=dff.index.max(),
-                                     start_date=dff.index.min()
+                                     start_date=dfh.index.min()
         ), className='six columns'),
         
     ], className='row'),
 
-    dcc.Graph(id='plot_comparison', figure={}),
-    dcc.Graph(id='plot_historical', figure={}),
+    dcc.Graph(id='plot-comparison', figure={}),
+    
+    
+    # dcc.Graph(id='plot-historical', figure={}),
+    
+    html.H1('///', style={"textAlign": "center"}), 
+    html.Div([
+        html.Div(dcc.Dropdown(
+            id='city-dropdown-multiple', value=['Athens', 'Berlin', 'Paris', 'Rome'], clearable=False,
+            options=[{'label': x, 'value': x} for x in sorted([i for i in dfh.columns])],
+            multi=True
+        ), className='six columns'),        
+    ], className='row'),
+    
+    dcc.Graph(id='boxplot-comparison', figure={}),
+    dcc.Graph(id='table-comparison-described', figure={}),
+    dcc.Graph(id='table-comparison-maes', figure={}),
+    dcc.Graph(id='choro-mae-map', figure={}),
+
+            
 ])
 
 @app.callback(
-    Output(component_id='plot_comparison', component_property='figure'),
+    Output(component_id='plot-comparison', component_property='figure'),
     Input(component_id='city-dropdown', component_property='value'),
     Input(component_id='date-range', component_property='start_date'),
     Input(component_id='date-range', component_property='end_date')
 )
-
 def comparison(city_chosen, start_date, end_date):
     # print(city_chosen)
-    print(start_date, end_date)
+    # print(start_date, end_date)
     all_value = [i for i in df_merged.columns if str(city_chosen) in i or 'query' in i]
     dropdown_value = [i for i in df_merged.columns if str(city_chosen) in i]
     # print(dropdown_value)
@@ -105,13 +122,8 @@ def comparison(city_chosen, start_date, end_date):
     df_merged_copy = df_merged_copy[all_value]
     
     
-    # print(start_date, end_date)
-    # print(df_merged_copy)
     df_merged_copy = df_merged_copy.loc[(df_merged_copy.index >= start_date) & (df_merged_copy.index <= end_date)]
     
-    # print(df_merged_copy)
-    # fig = px.line(df_merged_copy, x=df_merged_copy.index, y=dropdown_value, title='Historical-Forecast Comparison', markers=True, color='fc_query')
-    # fig.update_layout(yaxis_title='Comparison')
     
     
     # Using Plotly Graph Object
@@ -121,36 +133,114 @@ def comparison(city_chosen, start_date, end_date):
     fig.add_trace(go.Scatter(x=df_merged_copy.index, y=df_merged_copy[dropdown_value[1]],
                               mode='lines+markers', name=dropdown_value[1]))
     for query in df_merged_copy.fc_query.unique():
-        # df_merged_sliced = df_merged_copy.loc[df_merged_copy.fc_query == query]
-        # print(type(query))
         fig.add_vline(x=pd.to_datetime(query), line_width=2, line_dash="dash", line_color="green")
         
-        # fig.add_trace(go.Scatter(x=df_merged_sliced.index, y=df_merged_sliced[dropdown_value[0]],
-        #                           mode='lines', name=dropdown_value[0] + ' queried on' + query))
-        # fig.add_trace(go.Scatter(x=df_merged_sliced.index, y=df_merged_sliced[dropdown_value[1]],
-        #                           mode='lines', name=dropdown_value[1] + ' queried on' + query))
     
     
     return fig
 
+# @app.callback(
+#     Output(component_id='plot-historical', component_property='figure'),
+#     Input(component_id='city-dropdown', component_property='value'),
+#     Input(component_id='date-range', component_property='start_date'),
+#     Input(component_id='date-range', component_property='end_date')
+# )
+# def historical(city_chosen, start_date, end_date):
+#     # print(city_chosen)
+#     dropdown_value = [i for i in dfh.columns if str(city_chosen) in i]
+#     # print(dropdown_value)
+#     df_merged_copy = dfh.copy()
+#     df_merged_copy = dfh[dropdown_value]
+    
+#     df_merged_copy = df_merged_copy.loc[(df_merged_copy.index >= start_date) & (df_merged_copy.index <= end_date)]
+#     # print(df_merged_copy)
+#     fig = px.line(df_merged_copy, x=df_merged_copy.index, y=city_chosen, title='Historical Values', markers=True)
+#     fig.update_layout(yaxis_title='Historical Value')
+#     return fig
+
 @app.callback(
-    Output(component_id='plot_historical', component_property='figure'),
-    Input(component_id='city-dropdown', component_property='value'),
+    Output(component_id='boxplot-comparison', component_property='figure'),
+    Input(component_id='city-dropdown-multiple', component_property='value'),
     Input(component_id='date-range', component_property='start_date'),
     Input(component_id='date-range', component_property='end_date')
 )
-
-def historical(city_chosen, start_date, end_date):
-    # print(city_chosen)
-    dropdown_value = [i for i in df_merged.columns if str(city_chosen) in i]
-    # print(dropdown_value)
+def box_plot(city_list, start_date, end_date):
+    
+    dropdown_value = []
+    for city in list(city_list):  
+        dropdown_value_city = [i for i in df_merged.columns if str(city) == i]
+        dropdown_value = dropdown_value + dropdown_value_city
+    
     df_merged_copy = df_merged.copy()
     df_merged_copy = df_merged_copy[dropdown_value]
-    
     df_merged_copy = df_merged_copy.loc[(df_merged_copy.index >= start_date) & (df_merged_copy.index <= end_date)]
-    # print(df_merged_copy)
-    fig = px.line(df_merged_copy, x=df_merged_copy.index, y=city_chosen, title='Historical Values', markers=True)
-    fig.update_layout(yaxis_title='Athens Historical Value')
-    return fig
+    
+    box = go.Figure()
+    for city in dropdown_value:
+        box.add_trace(go.Box(y=df_merged_copy[city], name=city))
+    
+    box.update_layout(yaxis_title= str(dropdown_value) + 'Box Plot')
+        
+    return box
 
+@app.callback(
+    Output(component_id='table-comparison-described', component_property='figure'),
+    Output(component_id='table-comparison-maes', component_property='figure'),
+    Output(component_id='choro-mae-map', component_property='figure'),
+    Input(component_id='city-dropdown-multiple', component_property='value'),
+    Input(component_id='date-range', component_property='start_date'),
+    Input(component_id='date-range', component_property='end_date')
+)
+def table_statistics(city_list, start_date, end_date):
+    
+    dropdown_value = []
+    dropdown_value_mae = []
+    for city in list(city_list):  
+        dropdown_value_city = [i for i in df_merged.columns if str(city) in i]
+        dropdown_value = dropdown_value + dropdown_value_city
+        dropdown_value_mae.append(dropdown_value_city)
+                
+    df_merged_copy = df_merged.copy()
+    df_merged_copy = df_merged_copy[dropdown_value]
+    df_merged_copy = df_merged_copy.loc[(df_merged_copy.index >= start_date) & (df_merged_copy.index <= end_date)]
 
+    maes = {}
+    for city in list(dropdown_value_mae):
+        df_mae = df_merged_copy[city]
+        mae = abs(df_mae[city[0]]-df_mae[city[1]])
+        maes[city[0]] = round(mae.sum()/mae.count(), 2)
+        
+        
+    df_merged_copy_described = df_merged_copy.describe().round(2).reset_index()
+    df_maes = pd.DataFrame.from_dict(maes, orient='index', columns=['MAE']).reset_index()
+    df_maes_choro = df_maes.copy().rename(columns={'index':'city'})
+    
+    described = go.Figure(data=[go.Table(
+    header=dict(values=list(df_merged_copy_described.columns),
+                fill_color='paleturquoise',
+                align='left'),
+    cells=dict(values=[df_merged_copy_described[i] for i in df_merged_copy_described.columns],
+               fill_color='lavender',
+               align='left'))
+    ])
+    
+    maes = go.Figure(data=[go.Table(
+    header=dict(values=list(df_maes.columns),
+                fill_color='paleturquoise',
+                align='left'),
+    cells=dict(values=[df_maes[i] for i in df_maes.columns],
+               fill_color='lavender',
+               align='left'))
+    ])
+    
+    # Choropleth
+    for city, v in iso.items():  
+        df_maes_choro.loc[df_maes_choro.city==city, 'iso3'] =  v
+        
+    choro = px.choropleth(df_maes_choro, locations='iso3',
+                          color='MAE', hover_name='city',
+                          title='Choropleth MAE Map',
+                          color_continuous_scale=px.colors.sequential.Turbo)
+    choro.update_layout(margin=dict(l=60, r=60, t=50, b=50))
+    
+    return described, maes, choro

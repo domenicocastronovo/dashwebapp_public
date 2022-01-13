76066192d16fb9a95b3299a2af63b30f4fc797c3
Domenico <castronovo.d7@gmail.com>
2022-01-13 21:14:42 +0100

    daily data upload

--- apps/accuweather.py
+++ apps/accuweather.py
@@ -101,7 +101,7 @@ layout = html.Div([
                                      max_date_allowed=dff.index.max(),
                                      initial_visible_month=dff.index.min(),
                                      end_date=dff.index.max(),
-                                     start_date=dfh.index.max() - dt.timedelta(days=7)
+                                     start_date=dfh.index.max() - dt.timedelta(days=7*5)
         ), className='six columns'),
         
     ], className='row'),
